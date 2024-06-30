093e08f809e7562b84e77aa0c017b764f7f09aa3
Devang Pradeep <devangpradeep1@gmail.com>
2024-06-30 12:05:33 +0530

    feat: render-build

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_629_152_448) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_29_152448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,8 +28,8 @@ ActiveRecord::Schema[7.1].define(version: 20_240_629_152_448) do
     t.bigint "team_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index %w[team_id user_id], name: "index_teams_users_on_team_id_and_user_id"
-    t.index %w[user_id team_id], name: "index_teams_users_on_user_id_and_team_id", unique: true
+    t.index ["team_id", "user_id"], name: "index_teams_users_on_team_id_and_user_id"
+    t.index ["user_id", "team_id"], name: "index_teams_users_on_user_id_and_team_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
